96e351aa762c47ed386c9967b0da97a30f872938
Ravsanjani Rafli Arafat <rafliarafat12@gmail.com>
2024-05-14 10:18:56 +0700

    jobsheet 12 B. INFORMASI FILE UPLOAD

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -11,7 +11,7 @@ class FileUploadController extends Controller
     }
     public function prosesFileUpload(Request $request){
         // dump($request->berkas);
-        return "Pemrosesan file upload di sini";
+        // return "Pemrosesan file upload di sini";
 
         // $request->validate([
         //     'berkas'=>'required|file|image|max:5000',
@@ -27,20 +27,20 @@ class FileUploadController extends Controller
         // echo "Tampilan link: <a href='$pathBaru'>$pathBaru</a>";
         // echo $request->berkas->getClientOriginalName()." lolos validasi";
 
-        // if($request->hasFile('berkas')){
-        //     echo "path(): ". $request->berkas->path();
-        //     echo "<br>";
-        //     echo "extension(): ". $request->berkas->extension();
-        //     echo "<br>";
-        //     echo "getClientOriginalExtension(): ". $request->berkas->getClientOriginalExtension();
-        //     echo "<br>";
-        //     echo "getMimeType(): ". $request->berkas->getMimeType();
-        //     echo "<br>";
-        //     echo "getClientOriginalName(): ". $request->berkas->getClientOriginalName();
-        //     echo "<br>";
-        //     echo "getSize(): ". $request->berkas->getSize();
-        // } else {
-        //     echo "Tidak ada berkas yang diupload";
-        // }
+        if($request->hasFile('berkas')){
+            echo "path(): ". $request->berkas->path();
+            echo "<br>";
+            echo "extension(): ". $request->berkas->extension();
+            echo "<br>";
+            echo "getClientOriginalExtension(): ". $request->berkas->getClientOriginalExtension();
+            echo "<br>";
+            echo "getMimeType(): ". $request->berkas->getMimeType();
+            echo "<br>";
+            echo "getClientOriginalName(): ". $request->berkas->getClientOriginalName();
+            echo "<br>";
+            echo "getSize(): ". $request->berkas->getSize();
+        } else {
+            echo "Tidak ada berkas yang diupload";
+        }
     }
 }
