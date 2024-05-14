1373a265bcef96b33aca0f04850c6a43ad9a8714
Ravsanjani Rafli Arafat <rafliarafat12@gmail.com>
2024-05-14 10:20:44 +0700

    jobsheet 12 C. VALIDASI FILE UPLOAD

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -13,9 +13,9 @@ class FileUploadController extends Controller
         // dump($request->berkas);
         // return "Pemrosesan file upload di sini";
 
-        // $request->validate([
-        //     'berkas'=>'required|file|image|max:5000',
-        // ]);
+        $request->validate([
+            'berkas'=>'required|file|image|max:5000',
+        ]);
         // $path = $request->berkas->store('uploads');
         // $extFile = $request->berkas->getClientOriginalName();
         // $namaFile = 'web-'.time().".". $extFile;
@@ -25,22 +25,22 @@ class FileUploadController extends Controller
         // echo "Proses upload berhasil, file berada di: $path";
         // echo "<br>";
         // echo "Tampilan link: <a href='$pathBaru'>$pathBaru</a>";
-        // echo $request->berkas->getClientOriginalName()." lolos validasi";
+        echo $request->berkas->getClientOriginalName()." lolos validasi";
 
-        if($request->hasFile('berkas')){
-            echo "path(): ". $request->berkas->path();
-            echo "<br>";
-            echo "extension(): ". $request->berkas->extension();
-            echo "<br>";
-            echo "getClientOriginalExtension(): ". $request->berkas->getClientOriginalExtension();
-            echo "<br>";
-            echo "getMimeType(): ". $request->berkas->getMimeType();
-            echo "<br>";
-            echo "getClientOriginalName(): ". $request->berkas->getClientOriginalName();
-            echo "<br>";
-            echo "getSize(): ". $request->berkas->getSize();
-        } else {
-            echo "Tidak ada berkas yang diupload";
-        }
+        // if($request->hasFile('berkas')){
+        //     echo "path(): ". $request->berkas->path();
+        //     echo "<br>";
+        //     echo "extension(): ". $request->berkas->extension();
+        //     echo "<br>";
+        //     echo "getClientOriginalExtension(): ". $request->berkas->getClientOriginalExtension();
+        //     echo "<br>";
+        //     echo "getMimeType(): ". $request->berkas->getMimeType();
+        //     echo "<br>";
+        //     echo "getClientOriginalName(): ". $request->berkas->getClientOriginalName();
+        //     echo "<br>";
+        //     echo "getSize(): ". $request->berkas->getSize();
+        // } else {
+        //     echo "Tidak ada berkas yang diupload";
+        // }
     }
 }
